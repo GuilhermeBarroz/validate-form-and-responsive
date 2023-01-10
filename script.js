73e7cda8ba6c8cac8b2bf4b26f427299f002f25f
Guilherme Barros <guilhermebarroz@icloud.com>
2023-01-10 15:37:24 -0300

    updates

---
+++ script.js
@@ -0,0 +1,5 @@
+let name = document.getElementById("iname");
+let lastname = document.getElementById("ilastname ");
+let email = document.getElementById("iemail");
+let password = document.getElementById("ipassword");
+
